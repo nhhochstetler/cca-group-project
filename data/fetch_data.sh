f05a8d15b16add452f99ed4497984c050fc4b8dc
Nathaniel Hochstetler <nhh3@illinois.edu>
2019-03-21 20:53:46 +0000

    added script to get all data

---
+++ data/fetch_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-01.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-02.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-03.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-04.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-05.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-06.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-07.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-08.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-09.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-10.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-11.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2018-12.csv
+
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-01.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-02.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-03.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-04.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-05.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-06.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-07.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-08.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-09.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-10.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-11.csv
+wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-12.csv
