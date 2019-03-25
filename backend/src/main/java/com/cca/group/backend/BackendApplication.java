6356721d51adf6e8c82db22450237628ea3a0e12
Nate Hochstetler <nhhochstetler@crimson.ua.edu>
2019-03-25 19:02:30 -0400

    added in more code

--- backend/src/main/java/com/cca/group/backend/BackendApplication.java
+++ backend/src/main/java/com/cca/group/backend/BackendApplication.java
@@ -1,7 +1,6 @@
 package com.cca.group.backend;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -13,6 +12,10 @@ import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.ZooKeeperConnectionException;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class BackendApplication {
 
+	Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
@@ -32,6 +36,7 @@ public class BackendApplication {
 	
 	@Bean
 	public HBaseAdmin hbaseConfig() throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
+		logger.debug("Initializing HBase connection");
 		Configuration conf = HBaseConfiguration.create();
 		HBaseAdmin admin = new HBaseAdmin(conf);
 		
@@ -41,13 +46,48 @@ public class BackendApplication {
 	}
 	
 	private void createTable(HBaseAdmin admin) throws IOException {
-		HTableDescriptor taxiDescriptor = new HTableDescriptor(TableName.valueOf("taxi"));
-		taxiDescriptor.addFamily(new HColumnDescriptor("green"));
+		logger.debug("Creating the taxi table");
+		
+		HTableDescriptor taxiDescriptor = new HTableDescriptor(TableName.valueOf("green_taxi"));
+		
+		taxiDescriptor.addFamily(new HColumnDescriptor("vendorID"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("pickupTime"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("dropoffTime"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("storeFwdFlag"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("ratecodeID"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("pickupLocationID"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("dropoffLocationID"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("passengerCount"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("tripDistance"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("fareAmount"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("extra"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("mtaTax"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("tipAmount"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("tollsAmount"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("ehailFee"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("improvementSurcharge"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("totalAmount"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("paymentType"));
+		taxiDescriptor.addFamily(new HColumnDescriptor("tripType"));
+		
 		admin.createTable(taxiDescriptor);
 	}
 	
-	private void loadData() throws FileNotFoundException {
+	private void loadData() throws IOException {
+		logger.debug("Populating the taxi tables with data");
+		
+		//For now, just getting one months data
 		BufferedReader br = new BufferedReader(new FileReader("../data/green_tripdata_2018-01.csv"));
+		String input;
+		int count = 0;
+		while ((input = br.readLine()) != null) {
+			String[] vals = input.split(",");
+			
+			Put p = new Put(Bytes.toBytes("row" + count));
+			
+			System.out.println(input);
+			
+			count++;
+		}
 	}
-
 }
