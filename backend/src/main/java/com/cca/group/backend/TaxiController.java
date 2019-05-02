43a41334b5036bdd5a5a42f380619bd26d84b34e
Nate Hochstetler <nhhochstetler@crimson.ua.edu>
2019-05-02 19:10:04 -0400

    one more attempt with time

--- backend/src/main/java/com/cca/group/backend/TaxiController.java
+++ backend/src/main/java/com/cca/group/backend/TaxiController.java
@@ -48,7 +48,7 @@ public class TaxiController {
 			String dateValue = Bytes.toString(result.getValue(Bytes.toBytes("pickup"), Bytes.toBytes("pickupTime")));
 			String totalValue = Bytes.toString(result.getValue(Bytes.toBytes("cost_fees"), Bytes.toBytes("totalAmount")));
 			
-			LocalDate d = LocalDate.parse(dateValue);
+			LocalDate d = LocalDate.parse(dateValue.replace(" ", "T"));
 			
 			logger.debug("DATE {}", d.toString());
 			if (dateValue.split(" ")[0].equals(date)) {
